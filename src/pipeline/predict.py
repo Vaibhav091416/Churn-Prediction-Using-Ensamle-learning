06c44c262db8f0771321c6cd558403595b0f174f
Vaibhav091416 <vaibhavverma091416@gmail.com>
2024-12-30 13:49:01 +0530

    Fina Model Reay
    Final model ready.

---
+++ src/pipeline/predict.py
@@ -0,0 +1,18 @@
+# from flask import Flask,request,request_render_template
+# import numpy as np
+# import pandas as pd
+
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.pipeline import Pipeline
+
+# app=Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return request_render_template()
+
+# @app.route('/predictdata',methods=['GET','POST'])
+# def predict_data():
+#     if request.method=='GET':
+#         return request_render_template
+#     else:
